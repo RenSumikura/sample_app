08664bee4416c25f8fe6f77fed5fc407bea6c244
ren_sumikura <rsumikura@gmail.com>
2017-12-25 02:51:14 +0000

    Contact-1

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,22 @@
 class ContactsController < ApplicationController
-
-     def new
-          @contact = Contact.new
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+     @contact = Contact.new(contact_params)
+     if @contact.save
+          ContactMailer.sent(@contact).deliver 
+          flash[:success] = "Thanks!! We'll be in touch."
+          redirect_to root_url
      end
+  end
 
-     def create
-          @contact = Contact.new(contact_params)
-          if @contact.save
-               flash[:success] = "Thanks!! We'll be in touch."
-               redirect_to root_url
-          end
-     end
+  private
 
-     private
-
-          def contact_params
-               params.require(:contact).permit(:name, :email, :content)
-          end
-
+      def contact_params
+            params.require(:contact).permit(:name, :email, :content)
+      end
+  
+  
 end
